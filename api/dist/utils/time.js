d3cb8f3f99578e6d73a66e7b374b8e57892d9ed9
Pedro Montuani <pedrolucasmontuani.2000@gmail.com>
2023-10-12 21:31:18 -0300

    fix: removed dist folder

--- api/dist/utils/time.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getTimeDifference = void 0;
-const getTimeDifference = (x, y) => {
-    let diff = (x.getTime() - y.getTime()) / 1000;
-    diff /= 60;
-    return Math.abs(Math.round(diff));
-};
-exports.getTimeDifference = getTimeDifference;
